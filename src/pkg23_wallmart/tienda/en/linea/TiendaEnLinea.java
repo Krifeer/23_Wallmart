30dcb6b821b0002b9866177ca210f88a5b714fb7
h45ch <h45ch@192.168.56.1>
2026-08-06 22:50:18 -0600

    Primer commit

---
+++ src/pkg23_wallmart/tienda/en/linea/TiendaEnLinea.java
@@ -0,0 +1,16 @@
+package pkg23_wallmart.tienda.en.linea;
+
+/**
+ *
+ * @author h45ch
+ */
+public class TiendaEnLinea {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+    }
+    
+}
